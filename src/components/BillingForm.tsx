5c5cadda09c2321ec35dbb2162714eee2b250278
Mansur Isakov <mansur.i@payme.uz>
2024-01-14 15:51:40 +0500

    feat: add spaces

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -50,7 +50,7 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
             <CardTitle>Subscription Plan</CardTitle>
             <CardDescription>
               You are currently on the <strong>{subscriptionPlan.name}</strong>
-              plan.
+              &nbsp;plan.
             </CardDescription>
           </CardHeader>
 
@@ -68,7 +68,7 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
               <p className="rounded-full text-xs font-medium">
                 {subscriptionPlan.isCanceled
                   ? "Your plan will be canceled on "
-                  : "Your plan renews on"}
+                  : "Your plan renews on "}
                 {format(subscriptionPlan.stripeCurrentPeriodEnd!, "dd.MM.yyyy")}
                 .
               </p>
